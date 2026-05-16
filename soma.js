351ad83054a56d3cb63ef6866d91a19e8bab0e0e
sm6303045-prog <sm6303045@gmail.com>
2026-05-16 15:35:18 +0530

    Update soma.js

--- soma.js
+++ soma.js
@@ -1,3 +1,15 @@
 function changeBackground(){
-document.getElementById("body").style.backgroundColor="blue"
+    let backColor = generateecolorcode()
+document.getElementById("body").style.backgroundColor=backColor;
+}
+function generateecolorcode(){
+    let chars = ["0","1","2","3","4","5","6","7","8","9","a" ,"b","c","d","e","f"]
+    let color = "#"
+    for(let i = 0 ; i<6 ; i++){
+        let randomnumber = Math.random()*16
+        let index = Math.floor (randomnumber);
+        color = color+chars[index]
+
+    }
+    return color
 }
